e270cea37e357e6c10c6403280ba85db3b4bd5c1
HongDong12321 <inmagoghdalsrl@gmail.com>
2025-09-04 21:15:09 +0900

    1

---
+++ 7894.py
@@ -0,0 +1,13 @@
+import math
+
+repeat = int(input())
+
+for i in range(repeat):
+   
+   n = int(input())
+   log_sum = sum(math.log10(i) for i in range(1, n+1))
+   digit_count = int(log_sum) + 1
+   print(digit_count)  
+
+
+
