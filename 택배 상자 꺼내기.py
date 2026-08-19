87e6b404954cf1cf07334fb4cf138ef20a1e190f
MinGiHong0512 <heymingi0512@gmail.com>
2026-08-19 14:17:08 +0900

    택배 상자 꺼내기
    완료

---
+++ 택배 상자 꺼내기.py
@@ -0,0 +1,29 @@
+def solution(n, w, num):
+    answer = 0
+
+    n -= 1
+    num -=1
+
+    dir_top = 0
+    dir_bottom = 0
+
+    dir_top = n // w
+    dir_bottom = num // w
+
+    print(dir_top, dir_bottom)
+
+    answer = dir_top - dir_bottom
+
+    if dir_top % 2 == dir_bottom % 2:
+        if n % w >= num % w:
+            answer += 1
+    else:
+        if n % w + num % w >= w - 1:
+            answer+=1
+
+
+    return answer
+
+n, w, num = map(int, input().split())
+
+print(solution(n,w,num))
